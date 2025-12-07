f51da244c8d754c67ca058a913eefdf9525b22ce
Ahmed Ben Ahmed <ahmed.tanalt666@gmail.com>
2025-12-07 13:38:22 +0100

    build with tech collaboration page

---
+++ app/(root)/innovation-challenge/page.tsx
@@ -0,0 +1,12 @@
+import InnovationChallenge from "@/components/pages/InnovationChallenge"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Build With Tech",
+}
+
+const InnovationChallengePage = () => {
+    return <InnovationChallenge />
+}
+
+export default InnovationChallengePage
